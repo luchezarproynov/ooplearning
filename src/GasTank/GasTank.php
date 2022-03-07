2d3e5af9a34955053efdcd271121168dbf3fe855
Luchezar Proynov <luchezarproynov@gmail.com>
2022-03-07 14:05:38 +0200

    CS initial test

--- src/GasTank/GasTank.php
+++ src/GasTank/GasTank.php
@@ -5,49 +5,49 @@ namespace OOP\GasTank;
 use OOP\GasTank\GasDebit;
 use OOP\GasTank\DebitCalculator;
 
-
 class GasTank
 {
     use DebitCalculator;
-    
+
     /**
      * Gas tank total volume.
+     *
      * @var float
      */
-    private float $volume;
-    
+    private float $volume;     
+
     /**
-     * 
+     *
      * @var array
      */
     private array $debits;
-    
+
     public function __construct(float $volume)
     {
         $this->volume = $volume;
         $this->debits = [];
     }
-    
-    
+
+
     // OVERLOADING. Forbidden in PHP.
-//    public function __construct(float $volume, array $debits)
-//    {
-//        $this->volume = $volume;
-//        $this->debits = $debits;
-//    }
-    
-//    Final declare a method whitch cannot be inherited.
+    //    public function __construct(float $volume, array $debits)
+    //    {
+    //        $this->volume = $volume;
+    //        $this->debits = $debits;
+    //    }
+
+    //    Final declare a method whitch cannot be inherited.
     final public function getVolume(): float
     {
         return $this->volume;
     }
-    
+
     public function addDebit(GasDebit $debit): void
     {
         $this->validateInputDebit($debit);
         $this->debits[] = $debit;
     }
-    
+
     public function removeDebit(GasDebit $debit): void
     {
         foreach ($this->debits as $key => $localDebit) {
@@ -58,35 +58,35 @@ class GasTank
             }
         }
     }
-    
+
     public function getDebits(): array
     {
         return $this->debits;
     }
-    
+
     private function validateInputDebit(GasDebit $debit): void
     {
         if ($debit->volume > $this->volume) {
             throw new \Exception('Too big debit for the tank.');
         }
-        
+
         if ($debit->volume < 0) {
             throw new \Exception('Can\'t add negative number of debit.');
         }
     }
-    
+
     public function recalculateLastDebit(float $distance, float $usedFuelPer100Km): void
     {
         if (!is_array($this->debits) || empty($this->debits)) {
             return;
         }
-        
+
         $usedGas = $this->calculateUsedGas($distance, $usedFuelPer100Km);
         $currentDebit = end($this->debits)->volume;
         if ($usedGas > $currentDebit) {
             throw new \Exception('Too much gas used.');
         }
-        
+
         end($this->debits)->volume = $currentDebit - $usedGas;
     }
 }
